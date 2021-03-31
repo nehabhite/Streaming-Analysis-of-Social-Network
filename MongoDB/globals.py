706e0f00cbaafc0e27e0dc3c1a0aafebd94d1df2
Khushbu Patel <pkhush1993@gmail.com>
2021-04-01 05:18:03 +0530

    Update globals.py
    minor changes

--- MongoDB/globals.py
+++ MongoDB/globals.py
@@ -1,6 +1,7 @@
 import pymongo
 
 # TWITTER API KEYS
+# used to connect with twitter public API to fetch tweets
 CONSUMER_API_KEY = "MsR0xzen9YvoA8XDA1eJVdufy"
 CONSUMER_API_SECRET = "kMpRIH51FyvFgFu0ofR8rmLbqMnCSfLAFchYFnglDkYWDg5t5r"
 ACCESS_TOKEN = "137438589-H3540VwUJB4Ia8MSjoOinISV9oVeOjq2VDyEoGUQ"
